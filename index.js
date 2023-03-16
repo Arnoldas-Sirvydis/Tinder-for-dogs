1a56fea6887eaef9192302266f4f78deed62fb8d
Arnoldas-Sirvydis <arnoldas.sirvydis@gmail.com>
2023-03-16 14:47:08 +0200

    Swiping and liking dogs work now

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function getNextDog() {
 }
 
 
-const dog = getNextDog() ? new Dog(getNextDog()) : null
+let dog = getNextDog() ? new Dog(getNextDog()) : null
 
 function getDogHtml(dog) {
     return `
@@ -29,10 +29,16 @@ function getDogHtml(dog) {
 
 function render() {
     if (dog !== null && dog !== undefined) { 
-        profileHtml.innerHTML = getDogHtml(dog);
+        profileHtml.innerHTML = getDogHtml(dog)
+        hideBadges()
     } else {
         profileHtml.innerHTML = noMoreDogsHtml()
     }
+}
+
+function hideBadges() {
+    document.querySelector(".badge-nope").style.display = "none"
+    document.querySelector(".badge-like").style.display = "none"
 }
 
 function noMoreDogsHtml() {
@@ -52,13 +58,25 @@ function findDogMatch() {
 function swipeDog(dog) {
     dogs[findDogMatch(dog)].hasBeenSwiped = true
     document.querySelector(".badge-nope").style.display = "block"
+    setTimeout(() => updateDogAndRender(), 1000)
 }
 
 function likeDog(dog) {
     dogs[findDogMatch(dog)].hasBeenLiked = true
     document.querySelector(".badge-like").style.display = "block"
-}   
+    setTimeout(() => updateDogAndRender(), 1000)
+}
 
+function updateDogAndRender() {
+    const nextDog = getNextDog()
+    if (nextDog) {
+        dog = new Dog(nextDog)
+        render()
+    } else {
+        dog = null
+        render()
+    }
+}
 
 document.getElementById("button-swipe").addEventListener("click", () => swipeDog(dog))
 document.getElementById("button-like").addEventListener("click", () => likeDog(dog))
